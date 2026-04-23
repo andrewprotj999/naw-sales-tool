e4b7009768d1855947ae269f21c61c41768e3563
andrewprotj999 <259197925+andrewprotj999@users.noreply.github.com>
2026-04-22 20:37:41 -0400

    Make Pages app load tool directly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,33 +4,17 @@
  */
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
-function AppRoutes() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
-  const basePath = (import.meta.env.BASE_URL || "/").replace(/\/$/, "") || "/";
-
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <WouterRouter base={basePath === "/" ? "" : basePath}>
-            <AppRoutes />
-          </WouterRouter>
+          <Home />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
